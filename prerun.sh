203ddbc7a5867b5f0ef103439dbe6d7bc058304b
Ravid Cohen <ravid@lightricks.com>
2020-01-05 16:05:39 +0200

    ?

---
+++ prerun.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd external/DCNv2
+python setup.py build develop
+cd ~/notebooks/
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1t4BXWwKjTlqOQCIaTRlWYcT1pPhaZRUy' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1t4BXWwKjTlqOQCIaTRlWYcT1pPhaZRUy" -O yolact_plus_resnet50_54_800000.pth && rm -rf /tmp/cookies.txt
+mkdir models
+mv yolact_plus_resnet50_54_800000.pth models
