1a9ab21bf501622585a079e114b37eb3d53f1e05
Ravid Cohen <ravid@lightricks.com>
2020-01-07 11:59:33 +0200

    ?

--- train.py
+++ train.py
@@ -269,20 +269,21 @@ def train():
         for epoch in range(num_epochs):
             print('Epoch %d / %d' % (epoch, num_epochs))
             # Resume from start_iter
-            if (epoch+1)*epoch_size < iteration:
-                print(epoch, epoch_size, iteration)
-                print('max iter reached1')
-                continue
+            # if (epoch+1)*epoch_size < iteration:
+            #     print(epoch, epoch_size, iteration)
+            #     print('max iter reached1')
+            #     continue
             
             for datum in data_loader:
+                print('Iter', iteration)
                 # Stop if we've reached an epoch if we're resuming from start_iter
-                if iteration == (epoch+1)*epoch_size:
-                    print('max iter reached2')
-                    break
+                # if iteration == (epoch+1)*epoch_size:
+                #     print('max iter reached2')
+                #     break
 
                 # Stop at the configured number of iterations even if mid-epoch
-                if iteration == args.iters or cfg.max_iter:
-                    break
+                # if iteration == args.iters or cfg.max_iter:
+                #     break
 
                 # Change a config setting if we've reached the specified iteration
                 changed = False
