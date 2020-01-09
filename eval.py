65d6d6027f5dab4d9d654b8e0bf4bc206170fc45
Ravid Cohen <ravid@lightricks.com>
2020-01-09 19:12:04 +0200

    ?

--- eval.py
+++ eval.py
@@ -48,7 +48,7 @@ def parse_args(argv=None):
                         help='Further restrict the number of predictions to parse')
     parser.add_argument('--cuda', default=True, type=str2bool,
                         help='Use cuda to evaulate model')
-    parser.add_argument('--fast_nms', default=True, type=str2bool,
+    parser.add_argument('--fast_nms', default=False, type=str2bool,
                         help='Whether to use a faster, but not entirely correct version of NMS.')
     parser.add_argument('--cross_class_nms', default=False, type=str2bool,
                         help='Whether compute NMS cross-class or per-class.')
